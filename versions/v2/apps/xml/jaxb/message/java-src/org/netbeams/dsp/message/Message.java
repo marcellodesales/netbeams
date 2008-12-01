896a30f21b117f4fcaac28c44c4237b8359df01b
Kleber Sales <kleberkls@gmail.com>
2008-12-01 01:13:00 +0000

    Add isPojo util
    git-svn-id: svn://localhost/netbeams/trunk@98 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/xml/jaxb/message/java-src/org/netbeams/dsp/message/Message.java
+++ versions/v2/apps/xml/jaxb/message/java-src/org/netbeams/dsp/message/Message.java
@@ -3,4 +3,8 @@ package org.netbeams.dsp.message;
 
 public class Message extends AbstractMessage {
 
+	public static boolean isPojo(Object content){
+		return (content instanceof MessageContent);
+	}
+
 }

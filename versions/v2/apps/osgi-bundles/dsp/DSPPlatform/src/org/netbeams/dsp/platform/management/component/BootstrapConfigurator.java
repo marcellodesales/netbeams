57308438db0289bbcc8ec51357375666535b6d10
Kleber Sales <kleberkls@gmail.com>
2008-12-04 03:55:19 +0000

    Add the bootstrap message support
    git-svn-id: svn://localhost/netbeams/trunk@120 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPPlatform/src/org/netbeams/dsp/platform/management/component/BootstrapConfigurator.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPPlatform/src/org/netbeams/dsp/platform/management/component/BootstrapConfigurator.java
@@ -23,6 +23,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.netbeams.dsp.DSPException;
 import org.netbeams.dsp.MessageFactory;
 import org.netbeams.dsp.NodeAddressHelper;
@@ -34,22 +38,23 @@ public class BootstrapConfigurator {
 	
 	public static final String BOOTSTRAP_MESSAGE_DIR = "bootstrap";
 	
-	private static Map<String, Class<? extends Message>> prefixToCategory = new HashMap<String, Class<? extends Message>>();
-	static{
-		prefixToCategory.put("A", ActionMessage.class);
-		prefixToCategory.put("C", CreateMessage.class);
-		prefixToCategory.put("D", DeleteMessage.class);
-		prefixToCategory.put("E", EventMessage.class);
-		prefixToCategory.put("I", InsertMessage.class);
-		prefixToCategory.put("M", MeasureMessage.class);
-		prefixToCategory.put("Q", QueryMessage.class);
-		prefixToCategory.put("U", UpdateMessage.class);
-	}
+//	private static Map<String, Class<? extends Message>> prefixToCategory = new HashMap<String, Class<? extends Message>>();
+//	static{
+//		prefixToCategory.put("A", ActionMessage.class);
+//		prefixToCategory.put("C", CreateMessage.class);
+//		prefixToCategory.put("D", DeleteMessage.class);
+//		prefixToCategory.put("E", EventMessage.class);
+//		prefixToCategory.put("I", InsertMessage.class);
+//		prefixToCategory.put("M", MeasureMessage.class);
+//		prefixToCategory.put("Q", QueryMessage.class);
+//		prefixToCategory.put("U", UpdateMessage.class);
+//	}
 	
 	private String DSP_HOME;
 	
 	// <DSPComponent type> ==> <list of contend data files>
 	private Map<String, List<File>> contentDataFiles;
+	private File[] messageFiles;
 	
 	
 	public BootstrapConfigurator(String dspHome){
@@ -65,6 +70,7 @@ public class BootstrapConfigurator {
 		if(files != null){
 			Arrays.sort(files);
 			createTypeMap(files);
+			messageFiles = files;
 		}else{
 			Log.log("No files in " + dir.getAbsolutePath());
 		}
@@ -74,6 +80,23 @@ public class BootstrapConfigurator {
 		Log.log("BootstrapConfigurator.createMessages(): " + componentType);
 		
 		List<Message> messages = new ArrayList<Message>();
+		
+		List<File> files = contentDataFiles.get(componentType);
+		if(files != null){
+			for(File file: messageFiles){
+				try{
+					JAXBContext jc = JAXBContext.newInstance("org.netbeams.dsp.message", 
+							org.netbeams.dsp.message.ObjectFactory.class.getClassLoader());
+					Unmarshaller unmarshaller = jc.createUnmarshaller();
+					Message message = 
+							(MeasureMessage)unmarshaller.unmarshal(file);
+					messages.add(message);
+				}catch(JAXBException e){
+					Log.log(e);
+				}
+			}
+		}
+		
 		
 		/*
 		// Messages are supposed to be local. Create local consumers identifiers
@@ -106,9 +129,9 @@ public class BootstrapConfigurator {
 	}
 
 
-	private Class<? extends Message> getMessageClass(String[] nameParts) {
-		return prefixToCategory.get(nameParts[3]);
-	}
+//	private Class<? extends Message> getMessageClass(String[] nameParts) {
+//		return prefixToCategory.get(nameParts[3]);
+//	}
 
 	private byte[] readContentData(File file) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -130,10 +153,14 @@ public class BootstrapConfigurator {
 		return baos.toByteArray();
 	}
 
+	/**
+	 * Create a map from component type to File
+	 * @param files
+	 */
 	private void createTypeMap(File[] files) {
 		for(File file: files){
 			String name = file.getName();
-			// [<DSP component type>, <sequence>, <content data type>, <message category prefix>]
+			// [<DSP component type>_<sequence>]
 			String[] nameParts = parserFileName(name);
 			// Make sure the file name is valid
 			if(validateFileName(nameParts)){
@@ -146,17 +173,16 @@ public class BootstrapConfigurator {
 				Log.log("Add " + name + " to " + componentType);
 				dataFiles.add(file);
 			}else{
-				Log.log("Invalid content file name " + name);
+				Log.log("BootstrapConfigurator.Invalid content file name " + name);
 			}
-		}
-		
+		}		
 	}
 
 	private boolean validateFileName(String[] nameParts) {
-		if(nameParts == null || nameParts.length != 4){
+		if(nameParts == null || nameParts.length != 2){
 			return false;
 		}
-		for(int x=0; x<4; ++x){
+		for(int x=0; x<2; ++x){
 			if(nameParts[x] == null){
 				return false;
 			}
@@ -165,7 +191,7 @@ public class BootstrapConfigurator {
 	}
 
 	private String[] parserFileName(String name) {
-		String[] nameParts = new String[4];
+		String[] nameParts = new String[2];
 		// Remove .xml extension
 		int i = name.lastIndexOf('.');
 		if(i > -1){

748014cd9d15c25e7094b38e467b4b273880e9da
Kleber Sales <kleberkls@gmail.com>
2009-01-12 08:22:09 +0000

    Use log4j
    git-svn-id: svn://localhost/netbeams/trunk@135 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPStockConsumer/src/org/netbeams/dsp/demo/stocks/consumer/Activator.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPStockConsumer/src/org/netbeams/dsp/demo/stocks/consumer/Activator.java
@@ -1,14 +1,16 @@
 package org.netbeams.dsp.demo.stocks.consumer;
 
 
+import org.apache.log4j.Logger;
 import org.netbeams.dsp.platform.osgi.ActivatorHelper;
-import org.netbeams.dsp.util.Log;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
   
+	private static final Logger log = Logger.getLogger(Activator.class);
+
 	private BundleContext bundleContext;
 	private ServiceRegistration serviceRegistration;
 	
@@ -18,20 +20,24 @@ public class Activator implements BundleActivator {
    * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
    */
   public void start(BundleContext bundleContext) throws Exception {
-	  Log.log("StockConsumer.Activate.start()");
+	  log.info("start()");
 	  
 	  this.bundleContext = bundleContext;
 	  consumer = new StockConsumer();
 	  serviceRegistration = ActivatorHelper.registerOSGIService(bundleContext, consumer);
 
+	  log.info("start() completed");
   }
 
   /* (non-Javadoc)
    * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
    */
   public void stop(BundleContext context) throws Exception {
-	  Log.log("StockConsumer.Activator.stop()");
+	  log.info("stop()");
+	  
 	  ActivatorHelper.unregisterOSGIService(bundleContext, serviceRegistration);
 	  consumer.stopComponent();
+	  
+	  log.info("stop() completed");
   }
 }
--- versions/v2/apps/osgi-bundles/dsp/DSPStockConsumer/src/org/netbeams/dsp/demo/stocks/consumer/StockConsumer.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPStockConsumer/src/org/netbeams/dsp/demo/stocks/consumer/StockConsumer.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.log4j.Logger;
 import org.netbeams.dsp.ComponentDescriptor;
 import org.netbeams.dsp.message.ComponentLocator;
 import org.netbeams.dsp.DSPComponent;
@@ -17,11 +18,13 @@ import org.netbeams.dsp.demo.stock.StockTick;
 import org.netbeams.dsp.demo.stock.StockTicks;
 import org.netbeams.dsp.message.MeasureMessage;
 import org.netbeams.dsp.message.Message;
-import org.netbeams.dsp.util.Log;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 public class StockConsumer implements DSPComponent{
+	
+	
+	private static final Logger log = Logger.getLogger(StockConsumer.class);
 	
 	// Component Type
 	public final static String COMPONENT_TYPE = "org.netbeams.dsp.demo.stocks.consumer";
@@ -53,7 +56,8 @@ public class StockConsumer implements DSPComponent{
 
 	@Override
 	public void initComponent(String componentNodeId, DSPContext context) throws DSPException {
-		Log.log("StockConsumer.initComponent()");	
+		log.info("initComponent()");
+		
 		this.context = context;
 		this.componentNodeId = componentNodeId;
 	}
@@ -66,17 +70,17 @@ public class StockConsumer implements DSPComponent{
 
 	@Override
 	public void startComponent() {
-		Log.log("StockConsumer.startComponent()");
+		log.info("startComponent()");
 	}
 	
 	@Override
 	public void stopComponent() {
-		Log.log("StockConsumer.stopComponent()");
+		log.info("stopComponent()");
 	}
 
 	@Override
 	public void deliver(Message message) throws DSPException {
-		Log.log("StockConsumer.deliver()");
+		log.debug("deliver(Message)");
 		processMessage(message);
 	}
 
@@ -100,14 +104,14 @@ public class StockConsumer implements DSPComponent{
 
 
 	private void processMessage(Message message) {
-		Log.log("message.class=" + message.getClass().getName());
+		log.debug("message class=" + message.getClass().getName());
 		
 		if(message instanceof MeasureMessage){
 			
 			StockTicks stockTicks = null;
 			Object content = message.getBody().getAny();
 			if(Message.isPojo(content)){
-				Log.log("StockConsumer.processMessage(): Message Received....");
+				log.debug("StockConsumer.processMessage(): Message Received....");
 				return;
 				
 //				if(content instanceof StockTicks){
@@ -122,14 +126,14 @@ public class StockConsumer implements DSPComponent{
 					Unmarshaller unmarshaller = jc.createUnmarshaller();
 					stockTicks = (StockTicks)unmarshaller.unmarshal((Node)content);
 				}catch(JAXBException e){
-					Log.log(e);
+					log.error("Error unmarhalling the message", e);
 					return;
 				}
 
 			}
 			
 			for(StockTick tick: stockTicks.getStockTick()){
-				Log.log("Symble:" + tick.getSymbol() + "  " + "Value:" + tick.getValue());
+				log.debug("Symble:" + tick.getSymbol() + "  " + "Value:" + tick.getValue());
 			}
 		}
 	}

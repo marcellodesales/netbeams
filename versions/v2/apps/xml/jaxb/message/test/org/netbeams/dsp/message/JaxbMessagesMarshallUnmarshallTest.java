2f5a915be960b2a50cf4cfd17fdc19c8d055468b
Marcello de Sales <marcello.desales@gmail.com>
2009-01-18 06:50:51 +0000

    Issue 23
    Description:
    ------------
    
        Changing the XML Schema for the Messages Container to help identifying
        the host, using an interoperable date-time value, etc. 
        
        The addition of a DSPMessagesFactory to easy the construction of a DSP
        Message instance through easier shortcuts to be reused by any DSP
        data.
        
        This commit also adds a Test Case for the marshall and unmarshall of 
        messages as an example of how to use create a custom data type and
        integrate with the DSP framework. The sample should be integrated later
        to the bootstrap messages once they are defined.
    
    Details:
    ------------
    
     * messages.xsd
        - Adds the "host" attribute to the MessagesContainer to identify the
          host interested about the collection of messages on the container.
          Depending on the context (client-server interaction) the host might
          identify who needs to receive an acknowledgment message, or which
          DSP nodes need to be updated (server-clients).
        
     * DSPMessagesFactory
        - Offers easier factory methods to build a new DSP Messages. This first
          version only offers an instance of Measure message factory method.
        - Also includes factory methods for all the other parts of a DSP message.
          such as the header, component identification objects, etc.
        - It has the correct implementation of the way to set the body to the 
          DSP Message. 
          
        The JaxbMessagesMarshallUnmarshallTest class includes the examples for 
        the DSP components Stock Ticks and Mouse Actions.
    
     * generated-src
        - All the classes generated from the new version of the message.xsd 
          XML schema. They are still kept to easy development.
          
     * message.jar
        - Must be verified against the support of Java 5.
     
    Test Cases:
    ------------
    
     * org.netbeams.dsp.message.JaxbMessagesMarshallUnmarshallTest
        - It has the Marshalling tests and shows how DSP Component Developers
          must build the DSM message in order to support the serialization
          operations from the Wire Transport component. 
          
     * xml/samples/org.netbeams.dsp.message.bootstrap.xml
        - DSP Messages Container generated from the Marshalling test. 
    
    git-svn-id: svn://localhost/netbeams/trunk@140 559edde2-a4d4-44e0-96fc-4035e40d7396

---
+++ versions/v2/apps/xml/jaxb/message/test/org/netbeams/dsp/message/JaxbMessagesMarshallUnmarshallTest.java
@@ -0,0 +1,107 @@
+package org.netbeams.dsp.message;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.TestCase;
+
+import org.netbeams.dsp.demo.mouseactions.ButtonName;
+import org.netbeams.dsp.demo.mouseactions.EventName;
+import org.netbeams.dsp.demo.mouseactions.MouseAction;
+import org.netbeams.dsp.demo.mouseactions.MouseActionsContainer;
+import org.netbeams.dsp.demo.stock.StockTick;
+import org.netbeams.dsp.demo.stock.StockTicks;
+import org.netbeams.dsp.message.ComponentIdentifier;
+import org.netbeams.dsp.message.DSPMessagesFactory;
+import org.netbeams.dsp.message.Header;
+import org.netbeams.dsp.message.MeasureMessage;
+import org.netbeams.dsp.message.MessagesContainer;
+
+public class JaxbMessagesMarshallUnmarshallTest extends TestCase {
+
+    private DSPMessagesFactory fac;
+
+    public JaxbMessagesMarshallUnmarshallTest() {
+        this.fac = DSPMessagesFactory.INSTANCE;
+    }
+
+    public void testMarshallMessagesContainerWithDifferentMessages() {
+
+        ComponentIdentifier prod = this.fac.makeDSPComponentIdentifier("1", "LOCAL",
+                "org.netbeams.dsp.platform.management.component.ComponentManager");
+        ComponentIdentifier cons = this.fac.makeDSPComponentIdentifier(null, "LOCAL",
+                "org.netbeams.dsp.demo.stocks.consumer");
+        Header header = this.fac.makeDSPMessageHeader(null, System.currentTimeMillis(), prod, cons);
+
+        StockTicks ticks = new StockTicks();
+        StockTick tk1 = new StockTick();
+        tk1.setName("Google, Inc.");
+        tk1.setSymbol("GOOG");
+        tk1.setValue((float) 23.4);
+        ticks.getStockTick().add(tk1);
+
+        MeasureMessage stocks = null;
+        try {
+            stocks = this.fac.makeDSPMessage("1", "org.netbeams.dsp.demo.stock", header, ticks);
+        } catch (JAXBException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // ------------------ Creation of the Mouse Actions
+        ComponentIdentifier cons2 = this.fac.makeDSPComponentIdentifier(null, "192.168.0.101", "org.netbeams.dsp.mouseactions");
+        Header header2 = this.fac.makeDSPMessageHeader(null, System.currentTimeMillis(), prod, cons2);
+
+        MouseActionsContainer macontainer = new MouseActionsContainer();
+        MouseAction ma = new MouseAction();
+        ma.setButton(ButtonName.CENTER);
+        ma.setEvent(EventName.CLICKED);
+        ma.setValue("Button clicked at (3.5)"); 
+        ma.setX(3);
+        ma.setY(5);
+        MouseAction ma1 = new MouseAction();
+        ma1.setButton(ButtonName.NONE);
+        ma1.setEvent(EventName.MOVED);
+        ma1.setValue("Button moved at (4.7)");
+        ma1.setX(4);
+        ma1.setY(7);
+        macontainer.getMouseAction().add(ma);
+        macontainer.getMouseAction().add(ma1);
+
+        MeasureMessage mouseActions = null; 
+        try {
+            mouseActions = this.fac.makeDSPMessage("2", "org.netbeams.dsp.demo.mouseactions", header2, macontainer);
+        } catch (JAXBException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        MessagesContainer messages = this.fac.makeDSPMessagesContainer();
+        messages.getMessage().add(stocks);
+        messages.getMessage().add(mouseActions);
+
+        try {
+
+            JAXBContext context = JAXBContext.newInstance("org.netbeams.dsp.message");
+            Marshaller m = context.createMarshaller();
+            FileWriter fileWriter = new FileWriter(new File("/tmp/messagesExample.xml"));
+            m.marshal(messages, fileWriter);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+
+    }
+}

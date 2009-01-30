9c5517d59c1dad0487b0442fefcb6113e9663c09
Marcello de Sales <marcello.desales@gmail.com>
2009-01-30 17:57:56 +0000

    Issue 25
     Adding method to retrieve the correct IP address for
     the client. It's still not in used, but it has been
     tested.
    
    git-svn-id: svn://localhost/netbeams/trunk@214 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPWireTransport/src/org/netbeams/dsp/wiretransport/controller/DSPWireTransportHttpClient.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPWireTransport/src/org/netbeams/dsp/wiretransport/controller/DSPWireTransportHttpClient.java
@@ -7,7 +7,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -94,6 +99,43 @@ public class DSPWireTransportHttpClient implements DSPComponent {
         JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
         Unmarshaller um = context.createUnmarshaller();
         return (MessagesContainer) um.unmarshal(input);
+    }
+
+    /**
+     * @return the current environment's IP address, taking into account the Internet connection to any of the available
+     *         machine's Network interfaces. Examples of the outputs can be in octatos or in IPV6 format.
+     * 
+     *         fec0:0:0:9:213:e8ff:fef1:b717%4 siteLocal: true isLoopback: false isIPV6: true
+     *         ============================================ 130.212.150.216 <<<<<<<<<<<------------- This is the one we
+     *         want to grab so that we can. siteLocal: false address the DSP on the network. isLoopback: false isIPV6:
+     *         false ==> lo ============================================ 0:0:0:0:0:0:0:1%1 siteLocal: false isLoopback:
+     *         true isIPV6: true ============================================ 127.0.0.1 siteLocal: false isLoopback:
+     *         true isIPV6: false
+     */
+    public static String getCurrentEnvironmentNetworkIp() {
+        Enumeration<NetworkInterface> netInterfaces = null;
+        try {
+            netInterfaces = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            log.error("Somehow we have a socket error...");
+        }
+
+        while (netInterfaces.hasMoreElements()) {
+            NetworkInterface ni = netInterfaces.nextElement();
+            Enumeration<InetAddress> address = ni.getInetAddresses();
+            while (address.hasMoreElements()) {
+                InetAddress addr = address.nextElement();
+                if (!addr.isLoopbackAddress() && !addr.isSiteLocalAddress()
+                        && !(addr.getHostAddress().indexOf(":") > -1)) {
+                    return addr.getHostAddress();
+                }
+            }
+        }
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "127.0.0.1";
+        }
     }
 
     /**

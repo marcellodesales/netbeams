d1ffbc99dc92a53a81fb41cd47182c66f5ab77b0
Teresa Johnson <gamma.particle@gmail.com>
2009-02-09 08:27:33 +0000

    Made changes to reflect the new changes in StockProducer. This example models after StockProducer's use of the messaging API.
    git-svn-id: svn://localhost/netbeams/trunk@244 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPTestRandomNumGen/src/org/netbeams/dsp/test/rand/Activator.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPTestRandomNumGen/src/org/netbeams/dsp/test/rand/Activator.java
@@ -1,7 +1,6 @@
 package org.netbeams.dsp.test.rand;
 
 import org.apache.log4j.Logger;
-import org.netbeams.dsp.messagesdirectory.controller.DSPMessagesDirectory;
 import org.netbeams.dsp.platform.osgi.ActivatorHelper;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -16,32 +15,24 @@ import org.osgi.framework.ServiceRegistration;
 public class Activator implements BundleActivator {
 	
 	private static final Logger log = Logger.getLogger(Activator.class);
+	
 	private BundleContext bundleContext;
     private ServiceRegistration serviceRegistration;
+    
     private RandomProducer producer;
 
 	public void start(BundleContext bc) throws Exception {		
 		log.info("Starting...");
 		
 		bundleContext = bc;
-		
-        ServiceReference sr = bc.getServiceReference(DSPMessagesDirectory.class.getName());
-        DSPMessagesDirectory messagesQueue = (DSPMessagesDirectory) bc.getService(sr);
-        if (messagesQueue == null) {
-            log.error("The Messages Queue serice could not be retrieved from the reference of the OSGi platform!!");
-            throw new IllegalStateException(
-                    "The Messages Queue serice could not be retrieved from the reference of the OSGi platform!!");
-        }
-
-		producer = new RandomProducer(messagesQueue);
-		//producer.start();
+		producer = new RandomProducer();
 		serviceRegistration = ActivatorHelper.registerOSGIService(bc, producer);
 	}
 
 	public void stop(BundleContext bc) throws Exception {
 		log.info("Stopping...");
 		ActivatorHelper.unregisterOSGIService(bundleContext, serviceRegistration);
-		//producer.stopComponent();
+		producer.stopComponent();
 		//producer.stopThread();
 		//producer.join();
 		bundleContext = null;
--- versions/v2/apps/osgi-bundles/dsp/DSPTestRandomNumGen/src/org/netbeams/dsp/test/rand/RandomProducer.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPTestRandomNumGen/src/org/netbeams/dsp/test/rand/RandomProducer.java
@@ -1,57 +1,66 @@
 package org.netbeams.dsp.test.rand;
 
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.SecureRandom;
-import java.util.List;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.bind.Unmarshaller;
 
 import org.apache.log4j.Logger;
 import org.netbeams.dsp.ComponentDescriptor;
 import org.netbeams.dsp.DSPComponent;
 import org.netbeams.dsp.DSPContext;
 import org.netbeams.dsp.DSPException;
+import org.netbeams.dsp.MessageBrokerAccessor;
+import org.netbeams.dsp.MessageCategory;
 import org.netbeams.dsp.MessageFactory;
 import org.netbeams.dsp.data.property.DSProperties;
-import org.netbeams.dsp.data.property.DSProperty;
-import org.netbeams.dsp.demo.mouseactions.model.dsp.MouseActionDSPComponent;
-import org.netbeams.dsp.message.ComponentIdentifier;
-import org.netbeams.dsp.message.ComponentLocator;
-import org.netbeams.dsp.message.DSPMessagesFactory;
-import org.netbeams.dsp.message.Header;
+import org.netbeams.dsp.demo.stock.StockTick;
+import org.netbeams.dsp.demo.stock.StockTicks;
 import org.netbeams.dsp.message.MeasureMessage;
 import org.netbeams.dsp.message.Message;
-import org.netbeams.dsp.message.content.ObjectFactory;
-import org.netbeams.dsp.messagesdirectory.controller.DSPMessagesDirectory;
-import org.netbeams.dsp.wiretransport.controller.DSPWireTransportHttpClient;
+import org.netbeams.dsp.message.UpdateMessage;
+import org.w3c.dom.Node;
 
 
 public class RandomProducer implements DSPComponent {
 	
 	private static final Logger log = Logger.getLogger(RandomProducer.class);	
-	
-	private static ComponentDescriptor componentDescriptor;
-	private DSPContext context;
-    private String componentNodeId;
-    private ComponentLocator locator;
-    private ComponentDescriptor descriptor;
-    private List <RandomNumber> dataList;
-    private RandomNumberGenerator rng;
-
-	private DSPMessagesDirectory messagesQueueService;
-    
-    public static final String COMPONENT_TYPE = "org.netbeams.dsp.test.rand";
+	private static final ComponentDescriptor componentDescriptor;
+	public static final String COMPONENT_TYPE = "org.netbeams.dsp.test.rand";
 	public static final String MSG_CONTENT_TYPE_RANDOM_FLOAT = "random.float";
     
-    public RandomProducer(DSPMessagesDirectory messQueueService) {
-    	this.messagesQueueService = messQueueService;
-    };
-    
+	private DSPContext context;
+    private String componentNodeId;
+    private List <RandomNumber> dataList;
+    private RandomNumberGenerator rngThread;
         
+    public RandomProducer() {};
+    
+    static{
+		componentDescriptor = new ComponentDescriptor();
+		
+		Collection<MessageCategory> producedMessageCategories = new ArrayList<MessageCategory>();
+		Collection<MessageCategory> consumedMessageCategories = new ArrayList<MessageCategory>();
+		// StockTick
+		MessageCategory messageCategory = 
+			new MessageCategory(RandomProducer.class.getName(), MSG_CONTENT_TYPE_RANDOM_FLOAT);
+		
+		producedMessageCategories.add(messageCategory);
+		componentDescriptor.setMsgCategoryProduced(producedMessageCategories);
+		componentDescriptor.setMsgCategoryConsumed(consumedMessageCategories);
+		
+	}  
+    
+    
  // Implemented methods of the DSPComponent interface.
     
     public void deliver(Message message) throws DSPException {
@@ -74,16 +83,16 @@ public class RandomProducer implements DSPComponent {
 
 	public void startComponent() throws DSPException {
 		log.info("Starting component");
-		rng = new RandomNumberGenerator();
-		rng.start();
+		rngThread = new RandomNumberGenerator();
+		rngThread.start();
 	}
 
 	public void stopComponent() throws DSPException {
 		log.info("Stopping component");
 		
-		if (rng != null) {
-			rng.stopGenerator();
-			rng = null;
+		if (rngThread != null) {
+			rngThread.stopGenerator();
+			rngThread = null;
 		}
 	}
 
@@ -106,57 +115,29 @@ public class RandomProducer implements DSPComponent {
 	
 	private void send(RandomNumbers rNums) throws DSPException{
 		
-		log.debug("Send random number");
+		//Create the message
+		Message message = MessageFactory.newMessage(MeasureMessage.class, rNums, this);
+		log.debug("Send random numbers");
 				
 		if(rNums == null){
-			log.debug("No random number generated");
-		} else {
+			log.debug("No random numbers generated");
+		}else{
 			if(rNums.getRandomNumber() == null){
-				log.debug("No random number generated");
-			} else {
-				log.debug("Size of Random number set is: " + rNums.getRandomNumber().size());
+				log.debug("No random numbers generated");
+			}else{
+				log.debug("Random number size is " + rNums.getRandomNumber().size());
 			}
 		}
 		
-		String localIPAddress = DSPWireTransportHttpClient.getCurrentEnvironmentNetworkIp();
-        
-        log.debug("Packaging Random Numbers to be sent from " + localIPAddress);
-        log.debug("Total number of Mouse Actions: " + rNums.getRandomNumber().size());
-        
-        DSProperties randomNumbersData = new DSProperties();
-        for (RandomNumber r : rNums.getRandomNumber())
-        {
-        	DSProperty b = new DSProperty();
-        	b.setValue(String.valueOf(r.getValue()));
-        	randomNumbersData.getProperty().add(b);
-        }
-        ComponentIdentifier producer = DSPMessagesFactory.INSTANCE.makeDSPComponentIdentifier(
-                MouseActionDSPComponent.class.getName(), localIPAddress, randomNumbersData.getContentContextForJAXB());
-        ComponentIdentifier consumer = DSPMessagesFactory.INSTANCE.makeDSPComponentIdentifier("DSPWebLogger",
-                "130.212.214.119", null);
-        // Note that the consumer has the Wire Transport default DEMO IP as the destination.
-        
-        log.debug("Packaging Random Numbers to be sent to "
-                + consumer.getComponentLocator().getNodeAddress().getValue());
-        Header header = DSPMessagesFactory.INSTANCE.makeDSPMessageHeader(null, producer, consumer);
-        
-        try {
-            Message message = DSPMessagesFactory.INSTANCE.makeDSPMeasureMessage(header, randomNumbersData, org.netbeams.dsp.data.property.ObjectFactory.class);
-            messagesQueueService.addMessageToOutboundQueue(message);
-
-    		// Always check if there is a broker available
-//    		MessageBrokerAccessor messageBroker = context.getDataBroker();
-//    		if(messageBroker != null){
-//    			messageBroker.send(message);
-//    		}else{
-//    			log.debug("Message broker not available");
-//    		}
-            
-        } catch (JAXBException e) {
-            log.error(e.getMessage(), e);
-        } catch (ParserConfigurationException e) {
-            log.error(e.getMessage(), e);
-        }
+		// Always check if there is a broker available
+		MessageBrokerAccessor messageBroker = context.getDataBroker();
+		if(messageBroker != null){
+			messageBroker.send(message);
+		}else{
+			log.debug("Message broker not available");
+		}
+		
+		
 	}
 	
 	private class RandomNumberGenerator extends Thread{

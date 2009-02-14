f238db52023d32365f5e3fb42a23778b40df74fe
Teresa Johnson <gamma.particle@gmail.com>
2009-02-14 23:02:28 +0000

    Adding execution script that sets up the text-console Knopflerfish environment and runs the entire framework.
    git-svn-id: svn://localhost/netbeams/trunk@267 559edde2-a4d4-44e0-96fc-4035e40d7396

---
+++ versions/v2/thirdparty/knopflerfish/2.1.1/scripts/run-netbeams.sh
@@ -0,0 +1,30 @@
+#!/usr/local/bin/bash
+#####################################################################
+# To use script, please make sure the following is done:
+# 
+# 1. Modify the 1st line of this script, if Bash is located somewhere
+#    else on your filesytem.
+#
+# 2. Check to make your environment variables:
+#
+#		DSP_RUNTIME
+# 	    KNOPFLERFISH_HOME
+#
+#    are set.
+#
+# 2. Create a netbeams subdirectory in ${KNOPFLERFISH_HOME}/jars
+#
+# 3. Download netbeams.xargs from the repository and place it in 
+#	 ${KNOPFLERFISH_HOME}.
+#
+# 4. This script can be executed from within your HOME directory.
+#
+# 5. Make sure a general build of the sources have been completed.
+#
+#####################################################################
+
+cd ${KNOPFLERFISH_HOME}
+cp -r ${DSP_RUNTIME} ${KNOPFLERFISH_HOME}/jars/netbeams
+
+java -Dlog4j.configuration="file:${KNOPFLERFISH_HOME}/jars/netbeams/runtime/log4j.xml" -jar framework.jar -xargs netbeams.xargs
+

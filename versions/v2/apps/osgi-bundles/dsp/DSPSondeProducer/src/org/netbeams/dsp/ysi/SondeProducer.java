8811600664d2ddf1a7445a8683647de922cb830e
Teresa Johnson <gamma.particle@gmail.com>
2009-02-18 10:40:11 +0000

    Added libs/sonde.jar to Bundle-Classpath in Manifest.mf and included the jar in the bundle itself.
    git-svn-id: svn://localhost/netbeams/trunk@297 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPSondeProducer/src/org/netbeams/dsp/ysi/SondeProducer.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPSondeProducer/src/org/netbeams/dsp/ysi/SondeProducer.java
@@ -85,83 +85,17 @@ private static final Logger log = Logger.getLogger(SondeProducer.class);
 
 		private boolean running = true;
 		private SondeDataContainer sondeDataContainer;
+		private SondeTestData sondeTestData;
 		private List<SondeDataType> sondeData;
 		
 		public SondeHandler () {
 			sondeDataContainer = new SondeDataContainer();
+			sondeTestData = new SondeTestData();  // Get the test data
 			sondeData = sondeDataContainer.getSondeData();
 		};
 		
 		
-		public void generateTestValues() {
-			SondeDataType sondeDataType = new SondeDataType();
-			sondeDataType.setDate("11/26/08");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setTime("05:24:51");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setTemp("C");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setSpCond("22.69");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setCond("183.0");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setResist("175.0");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setSal("5704.66");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setPress("0.09");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setDepth("-0.00");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setDepth("-0.00");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setPH("8.22");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setPhmV("-94.00");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setODOSat("111.70");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setODOConc("9.63");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setTurbid("0.30");
-			sondeDataContainer.getSondeData().add(sondeDataType);
-			
-			sondeDataType = new SondeDataType();
-			sondeDataType.setBattery("10.10");
-			sondeDataContainer.getSondeData().add(sondeDataType);			
-		}
-		
-		
 		public void run() {			
-			generateTestValues(); // populate container with test values
 			while (running) {			
 				try {
 					send(sondeDataContainer);

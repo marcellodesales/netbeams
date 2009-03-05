03e2fe35934db5979f1ed57ba2ac675261ee1d91
Teresa Johnson <gamma.particle@gmail.com>
2009-03-05 11:10:39 +0000

    Modified the test data class to handle the new schema types.
    git-svn-id: svn://localhost/netbeams/trunk@345 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPSondeProducer/src/org/netbeams/dsp/ysi/SondeTestData.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPSondeProducer/src/org/netbeams/dsp/ysi/SondeTestData.java
@@ -1,6 +1,13 @@
 package org.netbeams.dsp.ysi;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
+import java.text.SimpleDateFormat;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.DatatypeConfigurationException;
 
 /**
  * 
@@ -12,10 +19,14 @@ public class SondeTestData {
 	private SondeDataContainer sdc;
 	private ArrayList<SondeDataType> list;
 	private SondeDataType sdt;
+	private static GregorianCalendar dateTime;
+	private Month m;
 		
 	public SondeTestData() {
+		m = new Month();
 		sdc = new SondeDataContainer();
 		list = new ArrayList<SondeDataType> ();
+		initDateTime();
 		initTestData();
 		sdc.sondeData = list;
 	};
@@ -23,8 +34,14 @@ public class SondeTestData {
 	public SondeDataContainer getSondeTestData() {
 		return sdc;
 	}
+		
+	private void initDateTime() {
+		dateTime = new GregorianCalendar();
+		dateTime.clear();
+		dateTime.set(2008, Calendar.DECEMBER, 2, 22, 48, 53);
+	}
 	
-	private void initTestData() {
+	private void initTestData() {;
 		setDataSet1();
 		setDataSet2();
 		setDataSet3();
@@ -32,63 +49,102 @@ public class SondeTestData {
 	
 	private void setDataSet1() {
 		sdt = new SondeDataType();
-		sdt.setDate("11/26/80");
-		sdt.setTime("05:24:51");
-		sdt.setTemp("22.69");
-		sdt.setSpCond("183.0");
-		sdt.setCond("175.0");
-		sdt.setResist("5704.66");
-		sdt.setSal("0.09");
-		sdt.setPress("-0.00");
-		sdt.setDepth("-0.00");
-		sdt.setPH("8.22");
-		sdt.setPhmV("-94.00");
-		sdt.setODOSat("111.70");
-		sdt.setODOConc("9.63");
-		sdt.setTurbid("0.30");
-		sdt.setBattery("10.10");
+		sdt.setDate(dateTime);
+		sdt.setTime(dateTime);
+		sdt.setTemp((float) 22.69);
+		sdt.setSpCond((float) 183.0);
+		sdt.setCond((float) 175.0);
+		sdt.setResist((float) 5704.66);
+		sdt.setSal((float) 0.09);
+		sdt.setPress((float) 0.00);
+		sdt.setDepth((float) 0.00);
+		sdt.setPH((float) 8.22);
+		sdt.setPhmV((float) -94.00);
+		sdt.setODOSat((float) 111.70);
+		sdt.setODOConc((float) 9.63);
+		sdt.setTurbid((float) 0.30);
+		sdt.setBattery((float) 10.10);
 		list.add(sdt);
 	}
 	
 	private void setDataSet2() {
 		sdt = new SondeDataType();
-		sdt.setDate("11/26/80");
-		sdt.setTime("05:30:52");
-		sdt.setTemp("22.69");
-		sdt.setSpCond("183.0");
-		sdt.setCond("175.0");
-		sdt.setResist("5710.39");
-		sdt.setSal("0.09");
-		sdt.setPress("-0.00");
-		sdt.setDepth("-0.00");
-		sdt.setPH("8.22");
-		sdt.setPhmV("-94.10");
-		sdt.setODOSat("111.60");
-		sdt.setTurbid("0.40");
-		sdt.setBattery("10.00");
+		sdt.setDate(dateTime);
+		sdt.setTime(dateTime);
+		sdt.setTemp((float) 22.69);
+		sdt.setSpCond((float) 183.0);
+		sdt.setCond((float) 175.0);
+		sdt.setResist((float) 5710.39);
+		sdt.setSal((float) 0.09);
+		sdt.setPress((float) 0.00);
+		sdt.setDepth((float) 0.00);
+		sdt.setPH((float) 8.22);
+		sdt.setPhmV((float) -94.10);
+		sdt.setODOSat((float) 111.60);
+		sdt.setTurbid((float) 0.40);
+		sdt.setBattery((float) 10.00);
 		list.add(sdt);
 	}
 	
 	private void setDataSet3() {
 		sdt = new SondeDataType();
-		sdt.setDate("11/26/80");
-		sdt.setTime("05:36:52");
-		sdt.setTemp("22.70");
-		sdt.setSpCond("183.0");
-		sdt.setCond("175.0");
-		sdt.setResist("5710.49");
-		sdt.setSal("0.09");
-		sdt.setPress("-0.00");
-		sdt.setDepth("-0.00");
-		sdt.setPH("8.22");
-		sdt.setPhmV("-94.00");
-		sdt.setODOSat("111.70");
-		sdt.setTurbid("0.40");
-		sdt.setBattery("10.10");
+		sdt.setDate(dateTime);
+		sdt.setTime(dateTime);
+		sdt.setTemp((float) 22.70);
+		sdt.setSpCond((float) 183.0);
+		sdt.setCond((float) 175.0);
+		sdt.setResist((float) 5710.49);
+		sdt.setSal((float) 0.09);
+		sdt.setPress((float) 0.00);
+		sdt.setDepth((float) 0.00);
+		sdt.setPH((float) 8.22);
+		sdt.setPhmV((float) -94.00);
+		sdt.setODOSat((float) 111.70);
+		sdt.setTurbid((float) 0.40);
+		sdt.setBattery((float) 10.10);
 		list.add(sdt);
 	}
 	
 	
+	
+	public String dateToString(GregorianCalendar cal) {
+		String result = "", temp = "", year = "", day = "";
+		Integer month = 0;
+		int i = 0;
+		
+		StringTokenizer st = new StringTokenizer(cal.getTime().toString());
+		while (st.hasMoreTokens()) {
+			i++;
+			temp = st.nextToken();
+			if (i == 2) {
+				month = (Integer) m.getMonth(temp);
+			}
+			if (i == 3) day = temp;
+			if (i == 6) year = temp;			
+		}
+		result = year + "/" + month + "/" + day;
+		return result;
+	}
+	
+	
+	
+	public String timeToString(GregorianCalendar cal) {
+		String result = "";
+		int i = 0;
+		
+		StringTokenizer st = new StringTokenizer(cal.getTime().toString());
+		while (st.hasMoreTokens()) {
+			i++;
+		    result = st.nextToken();
+		    if (i == 4 && result.charAt(2) == ':') {
+		        break;
+		    }
+		}
+		return result;
+	}
+	
+	
+	/*
 	public static void main(String[] args) {
 		SondeTestData testData = new SondeTestData();
 		SondeDataContainer container;
@@ -98,9 +154,10 @@ public class SondeTestData {
 		System.out.println("size: " + container.getSondeData().size()); 
 		System.out.println();
 		
+		// Verify that the data can be retrieved.
 		for (int i = 0; i < container.getSondeData().size(); i++) {
-			System.out.println("Date: " + container.getSondeData().get(i).getDate());
-			System.out.println("Time: " + container.getSondeData().get(i).getTime());
+			System.out.println("Date: " + testData.dateToString((GregorianCalendar) container.getSondeData().get(i).getDate()));
+			System.out.println("Time: " + testData.timeToString((GregorianCalendar) container.getSondeData().get(i).getTime()));
 			System.out.println("Temp: " + container.getSondeData().get(i).getTemp());
 			System.out.println("SpCond: " + container.getSondeData().get(i).getSpCond());
 			System.out.println("Cond: " + container.getSondeData().get(i).getCond());
@@ -115,6 +172,7 @@ public class SondeTestData {
 			System.out.println("Battery: " + container.getSondeData().get(i).getBattery());
 			System.out.println();
 			System.out.println();
-		}				
-	}		
+		}
+	}
+	*/		
 }

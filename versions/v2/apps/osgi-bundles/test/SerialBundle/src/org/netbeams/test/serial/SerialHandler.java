890f3a2bad2f6927d64d41590e6d8ade84a69ab1
Teresa Johnson <gamma.particle@gmail.com>
2009-03-06 07:55:17 +0000

    Submitted test bundle which confirms successful asynchronous communication between the simulator and a bundle.
    Tests the interval rate change from 5 seconds to 1 second: Pauses for the first 30 seconds while data is sent every 5 seconds. Then data is sent every 1 second.
    
    git-svn-id: svn://localhost/netbeams/trunk@350 559edde2-a4d4-44e0-96fc-4035e40d7396

---
+++ versions/v2/apps/osgi-bundles/test/SerialBundle/src/org/netbeams/test/serial/SerialHandler.java
@@ -0,0 +1,58 @@
+package org.netbeams.test.serial;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 
+ * @author Teresa L. Johnson <gamma.particle@gmail.com>
+ *
+ */
+public class SerialHandler {
+
+	private static final int BAUD_RATE = 9600;
+	private InputStream in;
+	private OutputStream out;
+	private SerialReader sr;
+	private SerialWriter sw;
+		
+	public SerialHandler() {
+		super();
+	};
+	
+	public void connect (String portName) throws Exception {
+
+		CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+		if (portIdentifier.isCurrentlyOwned()) {
+			System.err.println("Error: Port is currently in use");
+		} else {
+			CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+			if (commPort instanceof SerialPort) {
+				
+				SerialPort serialPort = (SerialPort) commPort;
+	            serialPort.setSerialPortParams(BAUD_RATE,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+
+	            in = serialPort.getInputStream();
+	            out = serialPort.getOutputStream();
+
+	            sr = new SerialReader(in);
+	    		sw = new SerialWriter(out);
+	    		
+	    		Thread srThread = new Thread(sr);
+	    		Thread swThread = new Thread(sw);
+	    		
+	    		srThread.start();
+	    		swThread.start();
+	            
+	            System.out.println("Success: Connection established");
+	        } else {
+	            System.out.println("Error: Only serial ports are handled.");
+	        }
+		}		        
+	}
+	
+}

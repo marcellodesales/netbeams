8d1b1ade257738e39936637710ccf1604e6333a3
Kleber Sales <kleberkls@gmail.com>
2009-05-01 22:05:20 +0000

    git-svn-id: svn://localhost/netbeams/trunk@497 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/v2/apps/osgi-bundles/dsp/DSPFramework/src/org/netbeams/dsp/NodeAddressHelper.java
+++ versions/v2/apps/osgi-bundles/dsp/DSPFramework/src/org/netbeams/dsp/NodeAddressHelper.java
@@ -1,18 +1,40 @@
 package org.netbeams.dsp;
 
 import org.netbeams.dsp.message.NodeAddress;
+import org.netbeams.dsp.util.NetworkUtil;
 
 public class NodeAddressHelper {
 	
 	public static String localAddressStr;
-
+	
 	public static final NodeAddress LOCAL_NODEADDRESS = new NodeAddress();
 	static {
 		LOCAL_NODEADDRESS.setValue("LOCAL");
 	}
-
-	public static final NodeAddress NO_ADDRESS = new NodeAddress();
+	
+	public static final NodeAddress REMOTE_ADDRESS = new NodeAddress();
 	static {
-		NO_ADDRESS.setValue("no-address");
+		REMOTE_ADDRESS.setValue("REMOTE");
+	}
+	
+	public static final NodeAddress NONE_ADDRESS = new NodeAddress();
+	static {
+		NONE_ADDRESS.setValue("NONE");
+	}		
+
+	public static final NodeAddress ANY_ADDRESS = new NodeAddress();
+	static {
+		ANY_ADDRESS.setValue("ANY");
+	}		
+	
+	
+	public static final NodeAddress SAME_ADDRESS = new NodeAddress();
+	static {
+		SAME_ADDRESS.setValue("SAME");
 	}	
+	
+	public static boolean isLocal(String address){
+		return "LOCAL".equalsIgnoreCase(address) || "LOCALHOST".equalsIgnoreCase(address) ||
+    	"127.0.0.1".equals(address) || address.equalsIgnoreCase(NetworkUtil.getCurrentEnvironmentNetworkIp());
+	}
 }

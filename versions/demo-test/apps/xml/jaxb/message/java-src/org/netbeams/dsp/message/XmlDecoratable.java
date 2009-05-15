8ba84503c437ee495b13c9af0d0228315d9dd835
Teresa Johnson <gamma.particle@gmail.com>
2009-05-15 21:56:40 +0000

    Removing this directory.  Eclipse tagged, but didn't branch a previous revision correctly.
    git-svn-id: svn://localhost/netbeams/trunk@555 559edde2-a4d4-44e0-96fc-4035e40d7396

--- versions/demo-test/apps/xml/jaxb/message/java-src/org/netbeams/dsp/message/XmlDecoratable.java
+++
@@ -1,9 +0,0 @@
-package org.netbeams.dsp.message;
-
-public interface XmlDecoratable {
-
-    /**
-     * @return the XML instance of a given class representation.
-     */
-    public String toXml();
-}
